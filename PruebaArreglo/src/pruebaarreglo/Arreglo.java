f9facb2cfbea6b59d37e968105c3ae579869ac06
ramalaniz <ramialanizfd@gmail.com>
2021-10-04 23:51:51 -0300

    tp5

--- PruebaArreglo/src/pruebaarreglo/Arreglo.java
+++ PruebaArreglo/src/pruebaarreglo/Arreglo.java
@@ -4,8 +4,7 @@ package pruebaarreglo;
 public class Arreglo {
     
     static void sumarLista(){
-        8755¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿1258
-        444{o¿¿9dkioo
+
     }
     
     static void buscarMayor(){
